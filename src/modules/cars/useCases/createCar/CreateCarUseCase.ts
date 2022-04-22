c016f7bc37d48377832183e4e67ed8814e9c9638
Felipe Monteiro <luizfelipe.p@live.com>
2022-04-22 11:57:39 -0300

    create car

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -0,0 +1,46 @@
+import { inject, injectable } from 'tsyringe';
+
+import { ICreateCarDTO } from '@modules/cars/dtos/ICreateCarsDTO';
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { AppError } from '@shared/errors/AppError';
+
+@injectable()
+class CreateCarUseCase {
+  constructor(
+    @inject('CarsRepository')
+    private carsRepository: ICarsRepository
+  ) {}
+
+  async execute({
+    name,
+    description,
+    daily_rate,
+    license_plate,
+    fine_amount,
+    brand,
+    category_id,
+  }: ICreateCarDTO): Promise<Car> {
+    const carAlreadyExists = await this.carsRepository.findByLicensePlate(
+      license_plate
+    );
+
+    if (carAlreadyExists) {
+      throw new AppError('Car already exists');
+    }
+
+    const car = this.carsRepository.create({
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      fine_amount,
+      brand,
+      category_id,
+    });
+
+    return car;
+  }
+}
+
+export { CreateCarUseCase };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,8 +2,10 @@ import { container } from 'tsyringe';
 
 import { UserRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CartRepository';
 import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
 import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
 import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
 
@@ -21,3 +23,5 @@ container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UserRepository
 );
+
+container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
